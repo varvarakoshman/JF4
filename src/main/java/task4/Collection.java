51ff6c1d82b53577100e0468fe2d5cbb665fc5e8
vkoschman <207955@niuitmo.ru>
2018-07-02 22:58:40 +0400

    final change(hope so)

--- src/main/java/task4/Collection.java
+++ src/main/java/task4/Collection.java
@@ -26,6 +26,7 @@ public class Collection implements Serializable {
         map.put(film, actors);
     }
 
+
     public void removeActor(String film, String actor) {
         if (map.containsKey(film)) {
             ArrayList<String> list = map.get(film);
@@ -36,5 +37,9 @@ public class Collection implements Serializable {
 
     public void removeFilm(String film) {
         map.remove(film);
+    }
+
+    public ArrayList<String> getActors(String film){
+        return map.get(film);
     }
 }
